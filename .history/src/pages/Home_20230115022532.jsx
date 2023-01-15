4fd5659d5492839062623a5e61daecc4727b6f41
Vasil <vaskodzhakov@gmail.com>
2023-01-15 02:26:01 +0000

    a
    a

--- .history/src/pages/Home_20230115022532.jsx
+++ .history/src/pages/Home_20230115022532.jsx
@@ -5,7 +5,7 @@ import.meta.env.VITE_GITHUB_TOKEN
 export default function Home() {
     return (
         <div>
-            <h1 className="text-5xl text-yellow-400 pb-12 text-center">Welcome to <b>GitHub Find!</b></h1>
+            <h1 className="text-5xl text-yellow-400 pb-15 text-center">Welcome to <b>GitHub Find</b></h1>
             
             {/* SEARCH COMPONENT */}
             <UserResults />
