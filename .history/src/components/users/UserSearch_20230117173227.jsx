c2e0e413115e2ea7fe858d2f0368a8a2d84ffe31
Vasil <vaskodzhakov@gmail.com>
2023-01-17 18:26:10 +0000

    r
    r

--- .history/src/components/users/UserSearch_20230117173227.jsx
+++ .history/src/components/users/UserSearch_20230117173227.jsx
@@ -4,34 +4,23 @@ export default function UserSearch() {
     const [text, setText] = useState('')
 
     const handleChange = (e) => setText(e.target.value)
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        if (text === '') {
-            alert('Please enter a GitHub user')
-        } else {
-            // todo - search users
-        }
-    }
-
     return (
         <div className="flex flex-col items-center justify-center">
             <div className='grid grid-cols-1 mb-8 gap-8 items-center justify-center'>
                 <div>
-                    <form onSubmit={handleSubmit}>
+                    <form>
                         <div className="form-control">
                             <div className="relative">
                                 <input type="text"
                                     className='w-full pr-40 bg-gray-200 input input-lg text-black rounded-2xl '
-                                    style={{ borderTopRightRadius: '0.99rem', borderBottomRightRadius: '0.99rem' }}
+                                    style={{borderTopRightRadius: '0.85rem', borderBottomRightRadius: '0.9rem'}}
                                     placeholder='Search'
                                     value={text}
                                     onChange={handleChange}
 
                                 />
 
-                                <button type='submit' className="absolute top-0 right-0 text-white rounded-l-none rounded-r-xl w-36 btn btn-primary btn-lg" style={{ borderTopRightRadius: '0.8rem', borderBottomRightRadius: '0.8rem' }}>Go</button>
+                                <button type='submit' className="absolute top-0 right-0 text-whi rounded-l-none rounded-r-xl w-36 btn btn-primary btn-lg" style={{borderTopRightRadius: '0.8rem', borderBottomRightRadius: '0.8rem'}}>Go</button>
                             </div>
                         </div>
                     </form>
